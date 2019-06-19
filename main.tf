fbcb1502354d52385e4875cf325c933743f72cdb
vemireddyp <49990790+vemireddyp@users.noreply.github.com>
2019-06-19 11:25:08 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,72 @@
+# Create VPC
+resource "aws_vpc" "vpc-test" {
+  cidr_block = "10.25.16.0/21"
+  enable_dns_hostnames = "true"
+  tags {
+    Name = "VPC-TEST"
+  }
+}
+#Create Subnets
+resource "aws_subnet" "subnet 1" {
+  vpc_id = "${aws_vpc.vpc-test.id}"
+  cidr_block = "10.25.16.0/23"
+ tags {
+   Name = "DMZ1-test"
+}
+}
+resource "aws_subnet" "subnet 2" 
+  vpc_id = "${aws_vpc.vpc-test.id}"
+  cidr_block = "10.25.18.0/23"
+ tags {
+   Name = "DMZ2-test"
+}
+}
+resource "aws_subnet" "subnet 3"
+  vpc_id = "${aws_vpc.vpc-test.id}"
+  cidr_block = "10.25.20.0/23"
+ tags {
+   Name = "Inside1-test"
+}
+}
+resource "aws_subnet" "subnet 4"
+  vpc_id = "${aws_vpc.vpc-test.id}"
+  cidr_block = "10.25.22.0/23"
+ tags {
+   Name = "Inside2-test"
+}
+}
+
+#Create internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc-test.id}"
+  tags {
+    Name = "IGW-test"
+    }
+  }
+#Create network ACLs
+resource "aws_network_acl" "nacl" {
+  vpc_id = "${aws_vpc.vpc-test.id}"
+
+  egress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "NetworkACL-test"
+  }
+}
+
