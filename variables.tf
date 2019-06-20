10afb595c76b7a9b8231adaea174c24e80c67678
vemireddyp <49990790+vemireddyp@users.noreply.github.com>
2019-06-20 11:27:30 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+#variable "AWS_ACCESS_KEY" {}
+#variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {}
 variable "KEY_PAIR" {}
 variable "INSTANCE_USERNAME" {default = "admin"}
