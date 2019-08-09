62d1d5bd9adc9b3b77cab40f7be7603ff4ce9d84
vemireddyp <49990790+vemireddyp@users.noreply.github.com>
2019-08-09 13:44:08 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -48,6 +48,7 @@ resource "aws_instance" "server" {
     winrm set winrm/config/winrs '@{MaxMemoryPerShellMB=”300″}'
     winrm set winrm/config '@{MaxTimeoutms=”1800000″}'
     winrm set winrm/config/service '@{AllowUnencrypted=”true”}'
+    winrm set winrm/config/client '@{TrustedHosts="*"}'
     winrm set winrm/config/service/auth ‘@{Basic=”true”}’
     netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
     netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
