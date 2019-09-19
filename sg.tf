17cb68d3308041f08bc1cc864f80fb34416c9417
vemireddyp <49990790+vemireddyp@users.noreply.github.com>
2019-09-19 16:21:49 +0100

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -24,6 +24,12 @@ to_port = 6556
 protocol = "tcp"
 cidr_blocks = ["10.25.16.0/21"]
 }  
+ingress {
+from_port = 3389
+to_port = 3389
+protocol = "tcp"
+cidr_blocks = ["94.125.134.81/32"]
+}  
 vpc_id = "vpc-62026006" 
 tags = {
 Name = "rdp"
