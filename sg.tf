c4bd74f9ba8af69981421608ea6f8f3a59db76ec
vemireddyp <49990790+vemireddyp@users.noreply.github.com>
2019-09-27 11:27:11 +0100

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -14,6 +14,12 @@ cidr_blocks = ["94.125.134.81/32"]
 }
 ingress {
 from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["35.180.88.59/23"]
+}  
+ingress {
+from_port = 0
 to_port = 6556
 protocol = "tcp"
 cidr_blocks = ["10.25.20.0/32"]
@@ -22,7 +28,7 @@ ingress {
 from_port = 0
 to_port = 6556
 protocol = "tcp"
-cidr_blocks = ["10.25.16.0/21"]
+cidr_blocks = ["10.25.16.0/23"]
 }  
 ingress {
 from_port = 3389
